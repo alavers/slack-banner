d12786cb4ef25ab2efdb3508f8ffc0c848c1af1d
Andrew Lavers <alavers@zendesk.com>
2020-04-07 11:25:58 -0400

    Updates (#1)
    * Update jade to pug
    
    * Update remaining dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,76 +1,66 @@
-'use strict';
+"use strict";
 
-var path = require('path');
+var path = require("path");
 
-module.exports = function(grunt) {
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+module.exports = function (grunt) {
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
-    grunt.initConfig({
+  grunt.initConfig({
+    dirTmp: ".tmp/",
+    dirRelease: "dist/",
+    dirDebug: "public/",
 
-        dirTmp: '.tmp/',
-        dirRelease: 'dist/',
-        dirDebug: 'public/',
-
-        concurrent: {
-            dev: ['nodemon:web', 'watch:dev'],
-            options: {
-                logConcurrentOutput: true
-            }
+    concurrent: {
+      dev: ["nodemon:web", "watch:dev"],
+      options: {
+        logConcurrentOutput: true,
+      },
+    },
+    nodemon: {
+      web: {
+        script: "server.js",
+        options: {
+          ignore: ["node_modules/**", "public/**", "test/**"],
         },
-        nodemon: {
-            web: {
-                script: 'server.js',
-                options: {
-                    ignore: ['node_modules/**', 'public/**', 'test/**']
-                }
-            }
+      },
+    },
+    uglify: {
+      prod: {
+        files: {
+          "<%= dirRelease %>main.js": "<%= dirTmp %>main.js",
         },
-        uglify: {
-            prod: {
-                files: {
-                    '<%= dirRelease %>main.js': '<%= dirTmp %>main.js'
-                }
-            }
+      },
+    },
+    browserify: {
+      dev: {
+        files: {
+          "<%= dirDebug %>main.js": "src/main.js",
         },
-        browserify: {
-            dev: {
-                files: {
-                    '<%= dirDebug %>main.js': 'src/main.js'
-                },
-                options: {
-                    debug: true
-                }
-            },
-            prod: {
-                files: {
-                    '<%= dirTmp %>main.js': 'src/main.js'
-                },
-                options: {}
-            }
+        options: {
+          debug: true,
         },
-        watch: {
-            dev: {
-                files: [
-                    './src/**/*.js', './src/**/*.less', './views/**/*.jade'
-                ],
-                tasks: [
-                    'browserify:dev'
-                ],
-                options: {
-                    livereload: true,
-                    spawn: false,
-                    atBegin: true
-                }
-            }
-        }
-    });
+      },
+      prod: {
+        files: {
+          "<%= dirTmp %>main.js": "src/main.js",
+        },
+        options: {},
+      },
+    },
+    watch: {
+      dev: {
+        files: ["./src/**/*.js", "./src/**/*.less", "./views/**/*.pug"],
+        tasks: ["browserify:dev"],
+        options: {
+          livereload: true,
+          spawn: false,
+          atBegin: true,
+        },
+      },
+    },
+  });
 
-    grunt.registerTask('dev', ['concurrent:dev']);
-    grunt.registerTask('release', [
-        'browserify:prod',
-        'uglify:prod'
-    ]);
-    grunt.registerTask('default', [
-        'dev'
-    ]);
+  grunt.registerTask("dev", ["concurrent:dev"]);
+  grunt.registerTask("release", ["browserify:prod", "uglify:prod"]);
+  grunt.registerTask("default", ["dev"]);
 };
--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var path = require('path');
+var path = require("path");
 
-var publicDir = (process.env.NODE_ENV === 'production') ? 'dist' : 'public';
+var publicDir = process.env.NODE_ENV === "production" ? "dist" : "public";
 app.use(express.static(path.join(__dirname, publicDir)));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.get('/', function(req, res) {
-    var emojis = ['thumbsup', 'smile', 'soccer'];
-    res.render('index', {
-        emojis: emojis
-    });
+app.get("/", function (req, res) {
+  var emojis = ["thumbsup", "smile", "soccer"];
+  res.render("index", {
+    emojis: emojis,
+  });
 });
 
 var port = process.env.PORT || 3000;
-var server = app.listen(port, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Listening at http://%s:%s', host, port);
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Listening at http://%s:%s", host, port);
 });
 
 module.exports = app;
